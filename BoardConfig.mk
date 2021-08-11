85d5493ef922c8ec61f45ebb15ea8f2937373be5
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-08-12 02:05:21 +0300

    daisy: Build more libs from source
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,19 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
+# Extra libs needed for crypto
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libicuuc \
+    libxml2 \
+    libion \
+    android.hidl.base@1.0
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT)/lib64/android.hidl.base@1.0.so
+
 # TWRP Configuration
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
