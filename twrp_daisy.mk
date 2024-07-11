db565f36630338d5a72d68e1802cba2e7ec0e6a7
lupesoltec <dev@lupesoltec.com.br>
2024-07-11 08:49:38 -0400

    daisy: Kanged some customizations from twrp sakura
    *source: https://github.com/aneeshsvha/device_xiaomi_sakura_twrp
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- twrp_daisy.mk
+++ twrp_daisy.mk
@@ -4,13 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := daisy
+
 # Inherit AOSP product makefiles
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Inherit from daisy device
 DEVICE_PATH := device/xiaomi/daisy
 $(call inherit-product, device/xiaomi/daisy/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
